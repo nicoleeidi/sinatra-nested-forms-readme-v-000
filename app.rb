162f24b644acf77428372cda5344240210bf197f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-16 15:12:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  post '/student' do
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
 
 end
